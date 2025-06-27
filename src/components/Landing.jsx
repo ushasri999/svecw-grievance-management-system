617e73f59e8892a652382819b3b884186be245aa
mademjayasri2744 <jayasrimadem98@gmail.com>
2025-06-27 13:51:40 +0530

    Update Landing.jsx

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../css/styles.css';
+//import '../css/styles.css';
 
 const Landing = () => {
   const navigate = useNavigate();
